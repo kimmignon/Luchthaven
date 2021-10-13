f4660e2e5b59a58c13f03f5568e00d5920df57a7
kimmi <kimmi@DESKTOP-NT9T8KH>
2021-10-13 18:26:42 +0200

    Add project files.

---
+++ Luchthaven/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luchthaven
+{
+    internal class Baan
+    {
+        public string Code;
+        public bool Vrij;
+
+        public Baan (string Code)
+        {
+            this.Code = Code;
+            this.Vrij = false;
+        }
+
+        public string getCode() { return this.Code; }
+        public bool getVrij() {  return this.Vrij; }
+
+
+
+
+
+    }
+}
